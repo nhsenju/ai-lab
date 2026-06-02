6c5acb8862a787f1d2b51269d734e06954dc0885
nhsenju <nuf.hos@gmail.com>
2026-06-02 20:14:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ def home():
         "status": "online",
         "message": "AI Lab attivo 🚀"
     }
+
+# 🔥 AGGIUNTA IMPORTANTE PER RENDER
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(app, host="0.0.0.0", port=port)
